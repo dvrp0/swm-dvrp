f1961b7003c4c991474ed4307e3bd664d0b04943
daveripper0020 <daveripper0020@gmail.com>
2023-04-19 06:11:47 +0900

    정적 어댑터로 교체

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import { getLogoCount } from "$lib/server/logo";
 import type { PageServerLoad } from "./$types";
 
+export const prerender = true;
+
 export const load: PageServerLoad = () => {
     return { count: getLogoCount() };
 }
